9958b770034cc21f367569cce978881634436b0d
WP2guspriamanwarasi <raywarasi@gmail.com>
2022-02-03 20:01:41 +0700

    first commint

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,26 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo" Nama saya micel indah valentina saya tinggal di karo, hobi saya habisin snack di warung mama, dan aku makannya dikit kalau di depan orang, tapi kalau udah gak ada orang makannya banyak,terimakasih";
+        echo"<h1></h1>";
+        echo"banyak yang bilang aku gak bisa terbang, ehh tapi bener sih gak bisa terbang, bukan karna aku berat ya tapi memang gak ada sayap nya, mikir dong";
+        echo "<h1></h1>";
+        echo "i love you micel indah valentina";
+
+        echo "<h1></h1>";
+        echo "yasudah deh kalau gak di jawab";
+
+        echo "<h1></h1>";
+        echo "cerita dong";
+
+        echo "<h1></h1>";
+        echo " apa aja";
+
+        echo "<h1></h1>";
+        echo "aku jadi ngerasa bersalah loh";
+
+    }
+}
